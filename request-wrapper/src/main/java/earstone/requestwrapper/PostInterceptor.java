3409e58311a0568f3578685ec82c85d2a3c61358
hsuk <hsuk@zoit.co.kr>
2024-01-17 17:44:50 +0900

    필요한 라이브러리 추가

--- request-wrapper/src/main/java/earstone/requestwrapper/PostInterceptor.java
+++ request-wrapper/src/main/java/earstone/requestwrapper/PostInterceptor.java
@@ -1,10 +1,12 @@
 package earstone.requestwrapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.apache.commons.io.IOUtils;
 
 @Slf4j
 public class PostInterceptor implements HandlerInterceptor {
@@ -12,6 +14,8 @@ public class PostInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("===================================Interceptor Start==================================");
+        ObjectMapper objectMapper = new ObjectMapper();
+
         return true;
     }
 
